d7a5563c7ac1affe6c90573a1e7faba0503ca3e5
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-01-02 05:15:45 +0530

    Added rename and delete functions and added License

--- README.md
+++ README.md
@@ -4,9 +4,35 @@
 
 The Prolog program simulates a music playlist app, allowing users to create and manage custom playlists. Each playlist can contain multiple songs, and the program supports operations such as creating playlists, adding songs to existing playlists, and displaying the contents of a playlist.
 
+## Features
+
+- Create and manage playlists.
+- Add songs to playlists with details like title, artist, album, and year.
+- Display the list of songs in a playlist.
+- Rename playlists.
+- Delete playlists.
+
 ## Prerequisites
 
 To run this Prolog program, you need a Prolog interpreter installed on your machine. You can use popular Prolog interpreters such as SWI-Prolog.
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/Vikranth3140/Music-Playlist.git
+   ```
+
+2. Change directory to the music player:
+   ```bash
+   cd Music-Playlist
+   ```
+
+3. Load the Prolog code in your Prolog interpreter:
+   ```bash
+   ?- [playlist].
+   ```
 
 ## Usage
 
@@ -22,7 +48,7 @@ To run this Prolog program, you need a Prolog interpreter installed on your mach
 
 2. **Adding a Song to a Playlist:**
 
-   To add a song to an existing playlist, use the `add_song/4` predicate. For example:
+   To add a song to an existing playlist, use the `add_song/4` predicate.
 
    ```prolog
    ?- add_song('My Playlist', 'Song Title', 'Artist Name', UpdatedPlaylist).
@@ -32,13 +58,33 @@ To run this Prolog program, you need a Prolog interpreter installed on your mach
 
 3. **Displaying a Playlist:**
 
-   To display the contents of a playlist, use the `display_playlist/1` predicate. For example:
+   To display the contents of a playlist, use the `display_playlist/1` predicate.
 
    ```prolog
    ?- display_playlist('My Playlist').
    ```
 
    This will show the name of the playlist and a list of songs.
+
+4. **Renaming a Playlist:**
+
+   To rename the playlist, use the `rename_playlist/1` predicate.
+
+   ```prolog
+   ?- rename_playlist('Old Playlist', 'New Playlist').
+   ```
+
+   This will rename the name of the playlist.
+
+5. **Deleting a Playlist:**
+
+   To delete the playlist, use the `delete_playlist/1` predicate.
+
+   ```prolog
+   ?- delete_playlist('My Playlist').
+   ```
+
+   This will delete the playlist.
 
 ## Note
 
@@ -47,5 +93,22 @@ To run this Prolog program, you need a Prolog interpreter installed on your mach
 - The `fail` predicate is used to explicitly fail a predicate and force backtracking in case of failure.
 - Lists are used to store songs in each playlist, and the `append` predicate is used to concatenate lists when adding a new song.
 
+## How to Contribute
+
+Welcome to contribute to Prolog Music PLaylist App! Feel free to fork the repository and suggest any improvements. To contribute, follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and commit them.
+4. Push the changes to your fork.
+5. Submit a pull request.
+
+Thank you for your contributions!
+
+## License
+
+This project is licensed under the [MIT License](LICENSE.md).
+
 ## Author
+
 [Vikranth Udandarao](https://github.com/Vikranth3140)
--- musicplayer.pl
+++ musicplayer.pl
@@ -5,11 +5,11 @@
 playlist('My Playlist', []).
 
 % add_song predicate to add a new song to a playlist
-add_song(Name, Title, Artist, UpdatedPlaylist) :-
+add_song(Name, Title, Artist, Album, Year, UpdatedPlaylist) :-
     % Retrieving the old playlist using the playlist/2 predicate
     playlist(Name, OldPlaylist),
     % Appending the new song to the old playlist to create a new playlist
-    append(OldPlaylist, [[Title, Artist]], NewPlaylist),
+    append(OldPlaylist, [[Title, Artist, Album, Year]], NewPlaylist),
     % Removing the old playlist from the database
     retract(playlist(Name, OldPlaylist)),
     % Asserting the new playlist with the same name and updated list of songs
@@ -19,8 +19,57 @@ add_song(Name, Title, Artist, UpdatedPlaylist) :-
     % Using cut to prevent backtracking and ensuring that only one solution is found
     !.
 % If the add_song predicate fails, it returns false
-add_song(_, _, _, _) :-
+add_song(_, _, _, _, _, _) :-
+    write('Failed to add song. Please check your input.'), nl,
     fail.
+
+% create_playlist predicate to create a new playlist
+create_playlist(Name) :-
+    % Check if the playlist already exists
+    \+ playlist(Name, _),
+    % If not, assert a new empty playlist with the given name
+    asserta(playlist(Name, [])),
+    write('Playlist created successfully.'), nl,
+    !.
+% If the playlist already exists, display an error message
+create_playlist(Name) :-
+    write('Playlist with the name '), write(Name), write(' already exists.'), nl,
+    fail.
+
+% delete_playlist predicate to delete a playlist
+delete_playlist(Name) :-
+    % Check if the playlist exists
+    playlist(Name, _),
+    % If it exists, retract the playlist from the database
+    retract(playlist(Name, _)),
+    write('Playlist deleted successfully.'), nl,
+    !.
+% If the playlist doesn't exist, display an error message
+delete_playlist(Name) :-
+    write('Playlist with the name '), write(Name), write(' does not exist.'), nl,
+    fail.
+
+% rename_playlist predicate to rename a playlist
+rename_playlist(OldName, NewName) :-
+    % Check if the old playlist exists
+    playlist(OldName, OldPlaylist),
+    % Delete the old playlist
+    delete_playlist(OldName),
+    % Create a new playlist with the new name and the songs from the old playlist
+    create_playlist(NewName),
+    add_songs_to_playlist(NewName, OldPlaylist),
+    write('Playlist renamed successfully.'), nl,
+    !.
+% If the old playlist doesn't exist, display an error message
+rename_playlist(OldName, _) :-
+    write('Playlist with the name '), write(OldName), write(' does not exist.'), nl,
+    fail.
+
+% add_songs_to_playlist predicate to add a list of songs to a playlist
+add_songs_to_playlist(_, []).
+add_songs_to_playlist(Name, [[Title, Artist, Album, Year]|Rest]) :-
+    add_song(Name, Title, Artist, Album, Year, _),
+    add_songs_to_playlist(Name, Rest).
 
 % display_playlist predicate to display the list of songs in a playlist
 display_playlist(Name) :-
@@ -35,8 +84,9 @@ display_playlist(Name) :-
 
 % display_songs predicate to display each song in a list of songs
 display_songs([]).
-display_songs([[Title, Artist]|Rest]) :-
-    % Displaying the title and artist of the song
-    write('- '), write(Title), write(' by '), write(Artist), nl,
+display_songs([[Title, Artist, Album, Year]|Rest]) :-
+    % Displaying the title, artist, album, and year of the song
+    write('- '), write(Title), write(' by '), write(Artist),
+    write(' [Album: '), write(Album), write(', Year: '), write(Year), write(']'), nl,
     % Recursively calling the display_songs predicate for the rest of the list
     display_songs(Rest).
